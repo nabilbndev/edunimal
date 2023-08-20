bf0a099fea8c56aab8a8fd3fe7531197925dab9b
Nabil <nabilmahmudju49@gmail.com>
2023-08-20 11:05:35 +0600

    Added text to voice

--- lib/screens/upload_image_screen.dart
+++ lib/screens/upload_image_screen.dart
@@ -10,6 +10,7 @@ import 'package:image_picker/image_picker.dart';
 import 'package:edunimal/logic/signin_provider.dart';
 import 'package:provider/provider.dart';
 import '../components/upload_image/search_result_tile.dart';
+import 'package:flutter_tts/flutter_tts.dart';
 
 class UploadImagePage extends StatefulWidget {
   const UploadImagePage({super.key});
@@ -141,13 +142,82 @@ class _UploadImagePageState extends State<UploadImagePage> {
   }
 }
 
+// class StoryBoxAlertDialog extends StatelessWidget {
+//   const StoryBoxAlertDialog({
+//     super.key,
+//     required this.animalName,
+//   });
+
+//   final String animalName;
+
+//   @override
+//   Widget build(BuildContext context) {
+//     return AlertDialog(
+//       scrollable: true,
+//       title: Text('Story of a $animalName', textAlign: TextAlign.center),
+//       content: FutureBuilder<String>(
+//         future: generateStory(animalName),
+//         builder: (BuildContext context, AsyncSnapshot<String> snapshot) {
+//           if (snapshot.connectionState == ConnectionState.waiting) {
+//             return LinearProgressIndicator(
+//               color: brandColor,
+//             );
+//           } else {
+//             if (snapshot.hasError) {
+//               return const Text('Try Again');
+//             } else {
+//               return Text('${snapshot.data}');
+//             }
+//           }
+//         },
+//       ),
+//       actions: [
+//         TextButton(
+//           child: const Text('Close'),
+//           onPressed: () {
+//             Navigator.of(context).pop();
+//           },
+//         ),
+//       ],
+//     );
+//   }
+// }
+
 class StoryBoxAlertDialog extends StatelessWidget {
-  const StoryBoxAlertDialog({
+  StoryBoxAlertDialog({
     super.key,
     required this.animalName,
   });
 
   final String animalName;
+  final FlutterTts flutterTts = FlutterTts();
+
+  Future<void> textToSpeech(String story) async {
+    var dio = Dio();
+    var response = await dio.post(
+      'https://api.edenai.run/v2/audio/text_to_speech',
+      options: Options(
+        headers: {
+          'authorization':
+              'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyX2lkIjoiODU0NWFhNjItNjIxYy00NzNhLTkyNzktN2VmOWZhZjI1YjA5IiwidHlwZSI6ImFwaV90b2tlbiJ9.usMATUzTk828oKKulvS3zQHDzWChkZ_lYiA1POHcT_w',
+        },
+      ),
+      data: {
+        'providers': 'google',
+        'language': 'en',
+        'text': story,
+        'option': 'FEMALE',
+      },
+    );
+
+    if (response.statusCode == 200) {
+      // If server returns an OK response, parse the JSON.
+      await flutterTts.speak(story);
+    } else {
+      // If that response was not OK, throw an error.
+      throw Exception('Failed to load post');
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -165,7 +235,15 @@ class StoryBoxAlertDialog extends StatelessWidget {
             if (snapshot.hasError) {
               return const Text('Try Again');
             } else {
-              return Text('${snapshot.data}');
+              return Column(
+                children: [
+                  Text('${snapshot.data}'),
+                  IconButton(
+                    icon: const Icon(Icons.volume_up),
+                    onPressed: () => textToSpeech(snapshot.data!),
+                  ),
+                ],
+              );
             }
           }
         },
